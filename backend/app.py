c0a8fbb6f0613f15c152d3acbd2acc3c0a491366
Faikar Herzaman <f.fazazaman@gmail.com>
2025-04-28 15:32:42 -0400

    used database

--- backend/app.py
+++ backend/app.py
@@ -1,53 +1,176 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
 import json
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data', 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 CORS(app)
 
-DATA_FILE = 'data/user_stats.json'
-
-def load_stats():
-    with open(DATA_FILE, 'r') as f:
-        return json.load(f)
-def save_stats(stats):
-    with open(DATA_FILE, 'w') as f:
-        json.dump(stats, f, indent=2)
-
-@app.route('/api/stats', methods=['GET'])
-def get_stats():
-    stats = load_stats()
-    return jsonify(stats)
-
-@app.route('/api/stats', methods=['POST'])
-def post_stats():
-    stats = request.get_json()
-    save_stats(stats)
-    return {"message": "Stats saved successfully"}, 200
+db = SQLAlchemy(app)
 
 
-TASK_FILE = 'data/tasks.json' 
 
-def load_task():
-    with open(TASK_FILE, 'r') as f:
-        return json.load(f)
-def save_task(task):
-    with open(TASK_FILE, 'w') as f:
-        json.dump(task, f, indent=2)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    # relationship to tasks
+    tasks = db.relationship('Task', backref='user', lazy=True)
+
+    xp = db.Column(db.Integer, default = 0)
+    points = db.Column(db.Integer, default = 0)
+    level  = db.Column(db.Integer, default = 1)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.String(128))
+    deadline = db.Column(db.String(10))
+    priority = db.Column(db.String(10))
+    completed = db.Column(db.Boolean, default=False)
+    penalized = db.Column(db.Boolean, default=False)
+
+@app.route('/api/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    if not username or not password:
+        return jsonify({"error": "Please fill username and password"}), 400
+    if User.query.filter_by(username=username).first():
+        return jsonify({"error": "Username already taken"}), 409
+    new_user = User(username=username)
+    new_user.set_password(password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "User registered successfully", "user_id": new_user.id}), 201
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    if not username or not password:
+        return jsonify({"error": "Username and password are required"}), 400
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        return jsonify({
+            "message": "Login successful",
+            "user_id": user.id,
+            "username": user.username,
+            "points": user.points,
+            "xp": user.xp,
+            "level": user.level
+        }
+        ), 200
+    return jsonify({"error": "Invalid username or password"}), 401
+
+@app.route('/api/<int:user_id>/stats', methods=["GET"])
+def get_stats(user_id):
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+    tasks = Task.query.filter_by(user_id=user_id).all()
+
+    task_list = []
+    for task in tasks:
+        task_list.append({
+            "id": task.id,
+            "user_id": task.user.id,
+            "name": task.name,
+            "deadline": task.deadline,
+            "priority": task.priority,
+            "completed": task.completed,
+            "penalized": task.penalized
+        })
+    return jsonify({
+        "message": "Request successful",
+        "username": user.username,
+        "points": user.points,
+        "level": user.level,
+        "xp": user.xp,
+        "tasks": task_list
+        }), 200
+
+@app.route('/api/<int:user_id>/stats', methods=["POST"])
+def post_stats(user_id):
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
+    data = request.get_json()
+    user.points = data.get("points", user.points)
+    user.xp = data.get("xp", user.xp)
+    user.level = data.get("level", user.level)
+
+    db.session.commit()
+    
+    return jsonify({"message": "Stats updated successfully"}), 200
+
+@app.route('/api/<int:user_id>/tasks', methods = ['POST'])
+def post_tasks(user_id):
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+    
+    data = request.get_json()
+    name = data.get('name')
+    deadline = data.get('deadline')
+    priority = data.get('priority')
+
+    new_task = Task(
+        user_id=user_id,
+        name=name,
+        deadline=deadline,
+        priority=priority,
+        completed=False,
+        penalized=False
+    )
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return jsonify({"message": "Task successfully created", "task_id": new_task.id}), 201
+
+@app.route('/api/<int:user_id>/tasks/<int:task_id>', methods = ["PUT"])
+def update_task(user_id, task_id):
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+    task = Task.query.filter_by(id=task_id, user_id=user_id).first()
+    if not task:
+        return jsonify({"error": "Task not found"}), 404
+    
+    data = request.get_json()
+    task.name = data.get('name', task.name)
+    task.deadline = data.get('deadline', task.deadline)
+    task.priority = data.get('priority', task.priority)
+    task.completed = data.get('completed', task.completed)
+    task.penalized = data.get('penalized', task.penalized)
+
+    db.session.commit()
+
+    return jsonify({"message": "successfully updated task"}), 200
+        
+        
 
 
-@app.route('/api/tasks', methods=['GET'])
-def get_task():
-    task = load_task()
-    return jsonify(task)
-
-@app.route('/api/tasks', methods=["POST"])
-def update_task():
-    task = request.get_json()
-    save_task(task)
-    return {"message": "Task updated successfully"}, 200
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/app.js
+++ frontend/app.js
@@ -1,101 +1,218 @@
-async function loadStats() {
-    const res = await fetch("http://127.0.0.1:5000/api/stats");
-    const data = await res.json();
-    document.getElementById("point").textContent = data.points;
-    document.getElementById("level").textContent = "Level: " + data.level;
-    document.getElementById("progress-bar").textContent = "XP " + data.xp;
-}
+//REGISTRATION AND LOGIN
+async function register() {
+    const username = document.getElementById("reg-username").value;
+    const password = document.getElementById("reg-password").value;
 
-async function addPoints() {
-    const res = await fetch("http://127.0.0.1:5000/api/stats");
-    const data = await res.json();
-
-    data.points += 50;
-    data.xp += 5
-
-    if (data.xp >= 100) {
-        data.level += 1
-        data.xp = 0
-    }
-
-    await fetch("http://127.0.0.1:5000/api/stats", {
+    const res = await fetch('http://127.0.0.1:5000/api/register', {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify({username, password})
     });
-    loadStats()
+    const data = await res.json()
+    console.log(data);
+    if (res.ok) {
+        alert("✅ Registered! Now log in.");
+        window.location.href = "login.html";
+      } else {
+        alert(data.error)      }
+
 }
 
-async function loadTask() {
-    const res = await fetch("http://127.0.0.1:5000/api/tasks")
-    const tasks = await res.json();
 
+async function login() {
+    const username = document.getElementById("login-username").value;
+    const password = document.getElementById("login-password").value;
+
+    const res = await fetch('http://127.0.0.1:5000/api/login', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({username, password})
+    });
+
+    const data = await res.json()
+    if (res.ok) {
+        localStorage.setItem("user_id", data.user_id);
+        localStorage.setItem("username", data.username);
+        window.location.href = "index.html";
+        console.log("logged in");
+
+      } else {
+        alert(data.error);
+      }
+}
+
+
+//PAGE LOADING
+async function loadStats() {
+    const userId = localStorage.getItem("user_id");
+    if (!userId) {
+        alert("You're not logged in.");
+        window.location.href = "login.html";
+        return;
+    }
+
+    const res = await fetch(`http://127.0.0.1:5000/api/${userId}/stats`);
+    const data = await res.json();
+
+    if (!res.ok) {
+        alert(data.error || "Failed to fetch stats");
+        return;
+    }
+
+    document.getElementById("user-name").textContent = data.username;
+    document.getElementById("user-points").textContent = "Points: " + data.points;
+    document.getElementById("user-level").textContent = "Level: " + data.level;
+    document.getElementById("user-xp").textContent = "XP: " + data.xp;
+}
+
+async function loadTasks() {
+    const userId = localStorage.getItem("user_id");
+    if (!userId) {
+        alert("You're not logged in.");
+        window.location.href = "login.html";
+        return;
+    }
+    const res = await fetch(`http://127.0.0.1:5000/api/${userId}/stats`);
+    const data = await res.json();
+
+    if (!res.ok) {
+        alert(data.error || "Failed to fetch stats");
+        return;
+    }
     const ongoingTask = document.getElementById('ongoing-task');
     const completedTask = document.getElementById('completed-task');
+    const lateTask = document.getElementById('late-task');
+    const today = new Date();
 
-    ongoingTask.innerHTML = "";
-    completedTask.innerHTML = "";
-
-    for (const task of tasks) {
-        const taskElement = document.createElement("div");
-        taskElement.textContent = `${task.name}:\n${task.deadline}\n${task.priority}`
-
+    
+    for (const task of data.tasks) {
+        const deadline = new Date(task.deadline);
+        if (!task.completed && !task.penalized && today > deadline) {
+            task.penalized = true;
+            markTaskLate(task.id)
+            console.log(`Task ${task.name} is late`);
+        }
+        const taskElement = document.createElement('div');
+        taskElement.className = "task";
+        taskElement.textContent = task.name;
         if (task.completed) {
             completedTask.appendChild(taskElement);
-        }
-        else {
+        } 
+        else if (task.penalized) {
+            lateTask.appendChild(taskElement);
+        } else {
             ongoingTask.appendChild(taskElement);
         }
     }
 
 }
 
-async function createTask() {
-    console.log("called");
-    const name = document.getElementById('task-name').value;
-    const date = document.getElementById('task-deadline').value;
-    const priority = document.getElementById('task-priority').value;
+async function markTaskLate(taskId) {
+    const userId = localStorage.getItem('user_id');
 
-    if (!name || !date || !priority) 
-    {
-        alert("Please fill everything");
+    const res = await fetch(`http://127.0.0.1:5000/api/${userId}/tasks/${taskId}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            penalized: true
+        })
+    });
+
+    const data = await res.json();
+    if (!res.ok){
+        error(data.error);
+    }
+}
+
+
+//UPDATING DATA
+async function createTask() {
+    const userId = localStorage.getItem("user_id");
+    if (!userId) {
+        alert("You're not logged in.");
+        window.location.href = "login.html";
         return;
     }
+    const task_name = document.getElementById("task-name").value;
+    const task_deadline = document.getElementById("task-deadline").value;
+    const task_priority = document.getElementById("task-priority").value;
 
-    const newTask = {
-        id: Date.now(),
-        name: name,
-        deadline: date,
-        priority: priority,
-        completed: false
-    };
-
-    const res = await fetch("http://127.0.0.1:5000/api/tasks");
-    const tasks = await res.json();
-
-    tasks.push(newTask);
-
-    await fetch("http://127.0.0.1:5000/api/tasks", {
+    const res = await fetch(`http://127.0.0.1:5000/api/${userId}/tasks`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(tasks)
+        body: JSON.stringify({
+            "name": task_name,
+            "deadline": task_deadline,
+            "priority": task_priority
+        })
     });
 
-    document.getElementById('task-name').value ="";
-    document.getElementById('task-deadline').value = "";
-    document.getElementById('task-priority').value ="low";
-    
-    loadTask();
-    
 
+    if (!res.ok) {
+        const errorData = await res.json();
+        alert(errorData.error || "Failed to create task");
+        return;
+    }
+    
+    const data = await res.json();
+    console.log("Task created successfully:", data);
+    alert("Task created successfully!");
+    
 }
 
+async function addPoints() {
+    const userId = localStorage.getItem("user_id");
+    const res = await fetch(`http://127.0.0.1:5000/api/${userId}/stats`);
+    const data = await res.json();
+
+    if (!res.ok) {
+        alert(data.error || "Failed to fetch stats");
+        return;
+    }
+    data.points += 100;
+    data.xp += 10;
+
+    if (data.xp >= 100) {
+        data.level += 1;
+        data.xp = 0;
+    }
+    const updateRes = await fetch(`http://127.0.0.1:5000/api/${userId}/stats`, {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "points": data.points,
+            "xp": data.xp,
+            "level": data.level
+        })
+    });
+
+    const updateData = await updateRes.json();
+
+    if (!updateRes.ok) {
+        alert(updateData.error || "Failed to update stats");
+        return;
+    }
+}
+
+
+//PAGE HANDLING
+
 window.addEventListener("DOMContentLoaded", () => {
-    loadStats();
-    loadTask();
-  });
-  
+    const userId = localStorage.getItem("user_id");
+    if (userId) {
+        loadStats();
+        loadTasks();
+    } else {
+        window.location.href = "login.html";
+    }
+});
